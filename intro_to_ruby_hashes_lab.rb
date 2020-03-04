08917281846ed6397f85bbe1931b3bd865c7abf5
QIuyan Peng <nicolepeng99@gmail.com>
2020-03-04 17:35:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   my_dogs = {
-    "name" => "Grace Hopper", 
+    :name => "Grace Hopper", 
     "hight" => "2 inch"
   }
   my_dags["name"]
